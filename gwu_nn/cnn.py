4e78210e35231ee0f68f8aea507833c60e09897d
GrantMcClearn <grantmcc1@gwu.edu>
2020-11-16 01:28:54 -0500

    Add helper method; fix formatting.

--- gwu_nn/cnn.py
+++ gwu_nn/cnn.py
@@ -6,14 +6,27 @@ class CNN(GWUNetwork):
     def __init__(self):
         super().__init__()
 
-    # Slides the kernel over the 2d input and computes a weighted sum
     def conv_2d(self):
+        """Slides the kernel over the 2d input and computes a weighted sum"""
         pass
 
-    # Pools the data using an average.
     def average_pool(self):
+        """Pools the data using an average."""
         pass
 
-    # Flattens the data into a (1d) vector so as to maintain compatibility with the rest of the library.
     def flatten(self):
+        """Flattens the data into a (1d) vector so as to maintain compatibility with the rest of the library."""
         pass
+
+
+    def weighted_sum(self, mat1, mat2):
+        """Helper method to compute the weighted sum of two (sub-)matrices."""
+
+        # The matrices ought to be of the same size.
+        assert len(mat1) == len(mat2)
+        assert len(mat1[0]) == len(mat2[0])
+
+        ws = 0
+        for i in range(len(mat1)):
+            for j in range(len(mat1[0])):
+                ws += mat1[i][j] * mat2[i][j]
